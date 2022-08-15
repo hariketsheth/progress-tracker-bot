0ea52bee8cf674ca4c4b5eec6a8f6b38e207ec2c
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-08-15 15:28:26 +0530

    💁 Added `templates`

---
+++ helpers/templates.js
@@ -0,0 +1,36 @@
+module.exports = {
+  'standup': `
+\`\`\`
+Share your updates here so that the Leads and Team is aware about your progress. Respond to this discussion in the following format:
+
+**What did you achieve in the last 2 weeks?**:
+ -
+
+**What are your priorities for the next week?**:
+ -
+
+**Blockers**:
+ -
+
+**Shoutouts**:
+ - @username for x
+\`\`\`
+`,
+  'retrospective': `
+\`\`\`
+The goal of this meeting is to provide Technocrats members an opportunity to discuss what worked well in the last week and address any challenges, along with areas of improvement:
+
+**🟢 Green (All Good!)**:
+ -
+
+**🟡 Yellow (Use caution)**:
+ -
+
+**🔴 Red (Stop / need help)**:
+ -
+
+**📑 Action Items**:
+ -
+\`\`\`
+`,
+};
